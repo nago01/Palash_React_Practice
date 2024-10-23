2b658ce9d76190f69579a799368fbe64b385f3fc
nago01 <palashnagwa@gmail.com>
2024-10-24 02:47:10 +0530

    MemoryGame Final Code

--- src/MemoryGame.tsx
+++ src/MemoryGame.tsx
@@ -1,31 +1,46 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './MemoryGame.css';
 export const MemoryGame = () => {
        const [currValue, setCurrValue] = useState(-1);
        const [selectedIndex, setSelectedIndex] = useState([]);
-       let valueArray = [1,2,3,4,5,1,2,3,4,5];
+       const [prevIndex, setPrevIndex] = useState(-1);
+       const [showModal, setShowModal] = useState(false);
+       let valueArray = [1,2,3,4,5,1,2,3,4,5,6,7,7,6, 10, 10];
 
        const handleLogic = (item, index) => {
-         
-         console.log(currValue,'curr');
-         if(currValue ===  -1 || currValue === item){
+         if(currValue ===  -1){
+            
+            let newArr = [...selectedIndex];
+            setPrevIndex(index);
+            newArr.push(index);
+            setSelectedIndex([...newArr]);
             setCurrValue(item);
-            console.log("asd");
-         let newArr = [...selectedIndex];
-         newArr.push(index);
-         setSelectedIndex([...newArr]);
          }
-         else {
+         else if(currValue === item){
+           let newArr = [...selectedIndex];
+           setPrevIndex(index);
+           newArr.push(index);
+           setSelectedIndex([...newArr]);
+           setCurrValue(-1);
+         }
+         else if(item!==currValue) {
          let  newArr= [...selectedIndex];
-         console.log(index, 'index');
-         newArr.filter((arrIndex)=>{
-               return arrIndex!=index;
+         let ans = newArr.filter((arrIndex)=>{
+               return arrIndex!==prevIndex;
             });
-            console.log(newArr, 'newArr');
+            ans.push(index);
+            setPrevIndex(index);
             setCurrValue(item);
+            setSelectedIndex(ans);
          }
        }
-   
+       
+       useEffect(()=>{
+         if(selectedIndex.length === valueArray.length){
+            setShowModal(true);
+         }
+       },[selectedIndex]);
+
        return (
         <>
          <div className="mainContainer">
@@ -34,14 +49,27 @@ export const MemoryGame = () => {
                {
                   valueArray.map((item, index)=>{
                      return <div className="cellContainer" style={{"backgroundColor": selectedIndex.includes(index) ? "green": "black"}} key={`${index}-${item}`} onClick={()=>{
+                        if(selectedIndex.includes(index)){
+                           return;
+                        }
                        handleLogic(item, index);
                      }}>
-                        {item}
+                        <div style={{"color": selectedIndex.includes(index) ? "white": "black"}}>{item}</div>
                         </div>
                   })
                }
    
             </div>
+            {showModal && <div className = "modalContainer">
+               <div>
+               Game Over Please Try Again
+               </div>
+               <button onClick={()=>{
+                  window.location.reload();
+               }}>
+                  Reset
+               </button>
+               </div>}
          </div>
         </>
        );
